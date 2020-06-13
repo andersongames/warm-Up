62ad38e717ec0808c77aef32369a00484a98e25e
andersongames <ander.cal45@gmail.com>
2020-06-13 15:26:31 -0300

    create point items script

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -44,3 +44,46 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]") //seleciona a select com name=uf
     .addEventListener("change", getCities) //adiciona um ouvidor de eventos, quando mudar o valor da select, executa a função getCities (passada por referência)
+
+
+//itens de coleta
+const itemsToCollect = document.querySelectorAll(".items-grid li") //seleciona as li
+
+for (const item of itemsToCollect) { //adiciona um ouvidor de evento (clicar na li)
+    item.addEventListener("click", handleSelectedItem)  //evento = clicar na li
+}
+
+const collectedItems = document.querySelector("input[name=items]") //atribui os itens selecionados à input itens
+
+let selectedItems = [] //array para armazenar os itens selecionados
+
+function handleSelectedItem(event) { //função que é executada quando um item é selecionado
+    const itemLi = event.target //alvo do evento
+
+    itemLi.classList.toggle("selected") //adiona a classe selected ao alvo do evento (li)
+
+    const itemId = itemLi.dataset.id //pega o id da li selecionada (target)
+
+    //verificar se existem itens selecionados (armazenados no array selectedItems)
+    const alreadySelected = selectedItems.findIndex( item => { //acha o index dos itens selecionados
+        const itemFound = item == itemId //compara o index do item com o itemId com o id da li //será true se houver item no array e será false se não houver
+        return itemFound //retorna os itens encontrados no array
+    })
+
+    //se já estiver selecionado (return true), remover do array
+    if(alreadySelected >=0) { //se já existem itens selecionados
+        //tirar da seleção
+        const filteredItems = selectedItems.filter( item => { //pega cada item que vieram pela alreadySelected
+            const itemIsDifferent = item != itemId //compara  com os id das li (se forem iguais será false, se forem diferentes será true)
+            return itemIsDifferent //retorna os itens que são diferentes
+        })
+
+        selectedItems = filteredItems //atualiza o array selectedItens com os itens que retornaram
+
+    } else { //adicionar na seleção
+        selectedItems.push(itemId) //adicionao o item selecionado no array
+    }
+
+    collectedItems.value = selectedItems //atualiza o input itens com os itens selecionados
+}
+
