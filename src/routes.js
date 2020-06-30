5878bf64b11af8651525fc5a7d00befe3fe79aa2
andersongames <ander.cal45@gmail.com>
2020-06-30 14:55:56 -0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,13 @@ const routes = express()
 //habilita o use do req.body
 routes.use(express.urlencoded({ extended: true }))
 
+//configura template engine (nunjucks)
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: routes,
+    noCache: true
+})
+
 //configurar rotas da aplicação
 //home
 routes.get("/", (req, res) => {
@@ -11,8 +18,13 @@ routes.get("/", (req, res) => {
 })
 
 //create-point
-
+routes.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
 
 //search-results
+routes.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,40 +1,12 @@
 const express = require("express") //importa o express
 const server = express()
+const routes = require("./routes") //importa as rotas
 
 //configura pasta pública
 server.use(express.static("public"))
 
-//configura template engine (nunjucks)
-const nunjucks = require("nunjucks")
-nunjucks.configure("src/views", {
-    express: server,
-    noCache: true
-})
-
-/*TESTE COM ROTAS
-const routes = require("routes")
+//rotas
 server.use(routes)
-TESTE COM ROTAS*/
-
-//habilita o use do req.body
-server.use(express.urlencoded({ extended: true }))
-
-//configurar rotas da aplicação
-    //home
-    server.get("/", (req, res) => {
-        return res.render("index.html")
-    })
-
-    //create-point
-    server.get("/create-point", (req, res) => {
-        return res.render("create-point.html")
-    })
-    
-
-    //search-results
-    server.get("/search-results", (req, res) => {
-        return res.render("search-results.html")
-    })
 
 //liga o servidor
 server.listen(3000)
